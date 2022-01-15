9338f101bc3d6084d046d37fdd1c2adb761629e4
unknown <ggg7152@gmail.com>
2022-01-15 21:12:20 +0900

    글 리스트 생성 - state,map()이용

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // state 변수
+  let [title,changeTitle] = useState(['자바스크립트','노드js','리액트']);
+
   return (
-    // <div className="App">
-    //   <div className="nav">개발blog</div>
-
-    //   <div className="list"></div>
-    //     <h3>글</h3>
-    
-    // </div>
-
+    <div className="App">
+      
+      {/* nav-bar */}
       <div className="nav">개발blog</div>
 
-      <div className="list"></div>
-        <h3>글</h3
+      {/* list */}
+      { title.map((title)=>{
+          return (
+            <div className="list">
+              <h3> {title} <span>👍</span> </h3>
+              <p>날짜</p>
+              <hr></hr>
+            </div>
+            )
+          }
+        )
+      }
+    </div>
   );
 }
 
