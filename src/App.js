453a55395eadc0809af76064235f5d861ff25511
unknown <ggg7152@gmail.com>
2022-01-15 20:44:48 +0900

    nav bar 생성

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <div className="nav">개발blog</div>
+
+    //   <div className="list"></div>
+    //     <h3>글</h3>
+    
+    // </div>
+
+      <div className="nav">개발blog</div>
+
+      <div className="list"></div>
+        <h3>글</h3
+  );
+}
+
+export default App;
